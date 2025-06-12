7a7538d26a353faf93993e8ea2ccfa8038045ba3
lromes43 <romesluke@gmail.com>
2025-06-11 22:35:42 -0400

    Log Reg set 1-5, check sheet for data

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -104,7 +104,7 @@ print(logit2prob(logr, X))
 ###random state ensures same split
 #42 for odd, 43 for even
 #change test size based on runs
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .15 , random_state = 42) 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2 , random_state = 42) 
 
 ##fits the logistic regression model to training set
 logr.fit(X_train, y_train)
