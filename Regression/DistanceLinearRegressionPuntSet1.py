7f2d594b55adb88bc75361231ce93e625b5a6f03
lromes43 <romesluke@gmail.com>
2025-07-08 13:20:23 -0400

    started DWS

--- Regression/DistanceLinearRegressionPuntSet1.py
+++ Regression/DistanceLinearRegressionPuntSet1.py
@@ -5,8 +5,14 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from scipy import stats
 import matplotlib.pyplot as plt
+import statsmodels
+from statsmodels import api as sm
+from statsmodels import stats
+from statsmodels.stats.stattools import durbin_watson
 
 
 #Load in Data
@@ -26,22 +32,69 @@ y = df[['Distance']]
 print(X.columns)
 print(y.columns)
 
-##X_train, X_test, y_train, y_test = train_test_split(
-   ##     X, y, test_size=0.12, random_state=42)
+##Durbin Watson Statistic
 
-#Using the linearregression method to create a linear regression object
+X = sm.add_constant(X)
 
-regr = linear_model.LinearRegression()
-regr.fit(X,y)
+model = sm.OLS(y,X)
+results = model.fit()
 
-slope, intercept, r, p, std_err = stats.linregress(X, y)
+residuals = results.resid
 
-def myfunc(x):
- return slope * x + intercept
+dw_statistic = durbin_watson(residuals)
 
-mymodel = list(map(myfunc, x))
+print(f"Durbin-Watson statistic: {dw_statistic}")
 
-print(mymodel)
+##Variance Inflation Factor
+
+X = df[['PLocID', 'Snaptime', 'Practice', 'precipitation', 'Wind', 'Temp', 'H2F', 'PlayerIDLS', 'OP', 'PDate', 'Hang', 'SnapLocID', 'Turf', 'Game', 'Grass', 'PlayerIDP']]
+y = df[['Distance']]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=.2, random_state=42)
+
+print(X.shape)
+print(y.shape)
+test = LinearRegression()
+
+resultss = test.fit(X_train, y_train)
+
+y_pred = resultss.predict(X_test)
+print(y_pred)
+
+
+print(y_pred.shape)
+print(y_train.shape)
+
+y_pred_train = test.predict(X_train)
+y_pred_test = test.predict(X_test)
+
+test_residual = y_test - y_pred_test
+print(test_residual)
+
+print(isinstance(test_residual, pd.DataFrame))
+
+
+def DWS (test_residual ) : 
+    n = len(test_residual)
+    if n < 2:
+        print("Warning! Not enough residuals need at least two")
+        return None
+    for i in range(1,n):
+        numerator_sum =  (test_residual[i] - test_residual[i-1])**2
+    for i in range(n):
+        denomiator_sum = test_residual[i]**2
+    if denomiator_sum == 0:
+        print("Cannot Compute")
+    return numerator_sum / denomiator_sum
+
+dwsValue = DWS(test_residual)
+if dwsValue is not None:
+    print(f"Calculated DWS: {dwsValue}")
+
+
+
+
 
 
 
