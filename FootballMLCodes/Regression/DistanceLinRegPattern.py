a57e436626f40e89b3cff25b4cf0ae9a97493d8f
Luke Romes <romesluke@gmail.com>
2025-07-25 15:10:59 -0400

    Each Feature vs Distance Score

--- FootballMLCodes/Regression/DistanceLinRegPattern.py
+++ FootballMLCodes/Regression/DistanceLinRegPattern.py
@@ -77,93 +77,134 @@ regr5score = regr5.score(X5,y)
 print(f"Snaptime vs Distance Score: {regr5score}")
 
 
-
-
-'''
-
-
-
-
-
 #OP vs Distance
 plt.scatter(df['OP'],df['Distance'], color = 'blue')
 plt.xlabel('OP')
 plt.ylabel('Distance')
-plt.show() ##Relation
-X6 = df['OP']
+#plt.show() ##Relation
+X6 = df[['OP']]
 y = df['Distance']
+regr6 = linear_model.LinearRegression()
+regr6.fit(X6,y)
+regr6score = regr6.score(X6,y)
+print(f"OP vs Distance Score: {regr6score}")
+
 
 #H2F vs Distance
 plt.scatter(df['H2F'],df['Distance'], color = 'blue')
 plt.xlabel('H2F')
 plt.ylabel('Distance')
-plt.show() ##relation
-X7 = df['H2F']
+#plt.show() ##relation
+X7 = df[['H2F']]
 y = df['Distance']
+regr7 = linear_model.LinearRegression()
+regr7.fit(X7,y)
+regr7score = regr7.score(X7,y)
+print(f"H2F vs Distance Score: {regr7score}")
+
 
 #Hang vs Distance
 plt.scatter(df['Hang'],df['Distance'], color = 'blue')
 plt.xlabel('Hang')
 plt.ylabel('Distance')
-plt.show() ##relation
-X8 = df['Hang']
+#plt.show() ##relation
+X8 = df[['Hang']]
 y = df['Distance']
+regr8 = linear_model.LinearRegression()
+regr8.fit(X8,y)
+regr8score = regr8.score(X8,y)
+print(f"Hang vs Distance Score: {regr8score}")
+
 
 #Practice vs Distance
 plt.scatter(df['Practice'],df['Distance'], color = 'blue')
 plt.xlabel('Practice')
 plt.ylabel('Distance')
-plt.show() ##no relation
-X9 = df['Practice']
+#plt.show() ##no relation
+X9 = df[['Practice']]
 y = df['Distance']
+regr9 = linear_model.LinearRegression()
+regr9.fit(X9,y)
+regr9score = regr9.score(X9,y)
+print(f"Practice vs Distance Score: {regr9score}")
 
 ##Game vs Distance
 plt.scatter(df['Game'],df['Distance'], color = 'blue')
 plt.xlabel('Game')
 plt.ylabel('Distance')
-plt.show() ##maybe relation
-X10 = df['Game']
+#plt.show() ##maybe relation
+X10 = df[['Game']]
 y = df['Distance']
+regr10 = linear_model.LinearRegression()
+regr10.fit(X10,y)
+regr10score = regr10.score(X10,y)
+print(f"Game vs Distance Score: {regr10score}")
 
 #Precipitation vs Distance
 plt.scatter(df['precipitation'],df['Distance'], color = 'blue')
 plt.xlabel('precipitation')
 plt.ylabel('Distance')
-plt.show() ## maybe relation
-X11 = df['precipitation']
+#plt.show() ## maybe relation
+X11 = df[['precipitation']]
 y = df['Distance']
+regr11 = linear_model.LinearRegression()
+regr11.fit(X11,y)
+regr11score = regr11.score(X11,y)
+print(f"Precipitaton vs Distance Score: {regr11score}")
 
 ##Wind vs Distance
 plt.scatter(df['Wind'],df['Distance'], color = 'blue')
 plt.xlabel('Wind')
 plt.ylabel('Distance')
-plt.show() #no relation
-X12 = df['Wind']
+#plt.show() #no relation
+X12 = df[['Wind']]
 y = df['Distance']
-
+regr12 = linear_model.LinearRegression()
+regr12.fit(X12,y)
+regr12score = regr12.score(X12,y)
+print(f"Wind vs Distance Score: {regr12score}")
 
 ##Grass vs Distance
 plt.scatter(df['Grass'],df['Distance'], color = 'blue')
 plt.xlabel('Grass')
 plt.ylabel('Distance')
-plt.show() ##no relation
-X13 = df['Grass']
+#plt.show() ##no relation
+X13 = df[['Grass']]
 y = df['Distance']
+Regr13 = linear_model.LinearRegression()
+Regr13.fit(X13,y)
+Regr13Score = Regr13.score(X13,y)
+print(f"Grass vs Distance Score: {Regr13Score}")
+
 
 ##Turf vs Distance
 plt.scatter(df['Turf'],df['Distance'], color = 'blue')
 plt.xlabel('Turf')
 plt.ylabel('Distance')
-plt.show() ##no relation
-X14 = df['Turf']
+#plt.show() ##no relation
+X14 = df[['Turf']]
 y = df['Distance']
+Regr14 = linear_model.LinearRegression()
+Regr14.fit(X14,y)
+Regr14Score = Regr14.score(X14,y)
+print(f"Turf vs Distance Score: {Regr14Score}")
+
+
 
 ##Temp vs Distance
 plt.scatter(df['Temp'],df['Distance'], color = 'blue')
 plt.xlabel('Temp')
 plt.ylabel('Distance')
-plt.show() ##maybe relation
-X15 = df['Temp']
+#plt.show() ##maybe relation
+X15 = df[['Temp']]
 y = df['Distance']
+Regr15 = linear_model.LinearRegression()
+Regr15.fit(X15,y)
+Regr15Score = Regr15.score(X15,y)
+print(f"Temp vs Distance Score: {Regr15Score}")
+
+'''
+
+
 
 '''
