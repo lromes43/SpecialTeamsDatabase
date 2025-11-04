f27fd96ac364012808f4307c97e45cfb984f4a1a
Luke Romes <romesluke@gmail.com>
2025-11-04 18:01:24 -0500

    removed one file

--- Data/Database/DatabaseSizeQuery.sql.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    table_schema AS DB_Name,
-    ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS "Database Size Megabytes(MB)"
-FROM 
-    information_schema.TABLES 
-WHERE 
-    table_schema = 'football' 
-GROUP BY 
-    table_schema;
